0163cca6d742e0000c4c73285891c240ce052c00
chouchoui <14866249+chouchoui@users.noreply.github.com>
2025-07-14 18:50:48 +0900

    feat: Days 49~52 done

---
+++ 100DaysOfSwiftUI/Project 10/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,33 @@
+//
+//  Created on 2025/07/14 18:14.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @Bindable var order: Order
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ 100DaysOfSwiftUI/Project 10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2025/07/14 17:00.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
