60297d99f10e6bb602d4530d6cb9979f7b050cf4
chouchoui <14866249+chouchoui@users.noreply.github.com>
2025-07-14 16:27:51 +0900

    feat: Days 39~42 done

---
+++ 100DaysOfSwiftUI/Project 8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,11 @@
+//
+//  Created on 2025/07/14 14:22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ 100DaysOfSwiftUI/Project 8/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2025/07/14 13:55.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
