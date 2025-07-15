1ad9f54e5a7688924c456f192038e9005a172f65
chouchoui <14866249+chouchoui@users.noreply.github.com>
2025-07-15 18:33:58 +0900

    feat: Days 62~67 done

---
+++ 100DaysOfSwiftUI/Project 13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2025/07/15 15:05.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
